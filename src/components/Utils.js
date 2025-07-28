3ccd7dc6e07e4259036877e72078c3875862952d
weixiaoyi <2278095567@qq.com>
2025-07-28 14:47:11 +0800

    注释

--- src/components/Utils.js
+++ src/components/Utils.js
@@ -17,6 +17,10 @@ export default class Utils {
     this.ins._onErrorCallback = callback;
   }
 
+  /*
+   * 获取websocket连接状态
+   * Get websocket connection status
+   */
   getConnectStatus = async () => {
     return await this.ins._promiseWrapper((commonParams = {}) => {
       this.ins._ws.send({
@@ -26,6 +30,10 @@ export default class Utils {
     });
   };
 
+  /*
+   * 设置客户端title
+   * Set client title
+   */
   setTitle = async (title) => {
     return await this.ins._promiseWrapper((commonParams = {}) => {
       this.ins._ws.send({
@@ -36,6 +44,10 @@ export default class Utils {
     });
   };
 
+  /*
+   * 设置客户端主题色
+   * Set client theme color
+   */
   setThemeColor = async (themeColor) => {
     return await this.ins._promiseWrapper((commonParams = {}) => {
       this.ins._ws.send({
@@ -46,6 +58,10 @@ export default class Utils {
     });
   };
 
+  /*
+   * 切换客户端Tabs的显隐
+   * Switch the visibility of client Tabs
+   */
   switchTabsVisibility = async (tabsVisibility) => {
     return await this.ins._promiseWrapper((commonParams = {}) => {
       this.ins._ws.send({
@@ -56,6 +72,10 @@ export default class Utils {
     });
   };
 
+  /*
+   * 设置“联系我们”页面的html内容
+   * Set the HTML content for the 'Contact Us' page
+   */
   setContactUsTabInnerHtml = async (html) => {
     return await this.ins._promiseWrapper((commonParams = {}) => {
       this.ins._ws.send({
